da9d8be8c935f3106e501684b966a786037b2951
Pheonixauto <95474402+Pheonixauto@users.noreply.github.com>
2023-03-24 17:40:15 +0700

    3 files modified, 8 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 import { AppRoutingModule } from './app-routing.module';
 import { MessagesComponent } from './share/component/messages/messages.component';
+import { NavbarComponent } from './share/component/navbar/navbar.component';
+import { SidebarComponent } from './share/component/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, FormsModule, AppRoutingModule,MessagesComponent],
+  imports: [BrowserModule, FormsModule, AppRoutingModule,MessagesComponent,NavbarComponent, SidebarComponent],
   providers: [],
   bootstrap: [AppComponent],
 })
