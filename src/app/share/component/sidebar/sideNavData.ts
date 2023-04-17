337d2ad6169bc9a325a98109e45b4186e4b94f78
Pheonixauto <95474402+Pheonixauto@users.noreply.github.com>
2023-04-17 17:27:40 +0700

    5 files modified, 1 added

---
+++ src/app/share/component/sidebar/sideNavData.ts
@@ -0,0 +1,12 @@
+export const sideNavData = [
+  {
+    routerLink: '/vegetable',
+    icon: '',
+    label: 'Vegetable',
+  },
+  {
+    routerLink: '/home',
+    icon: '',
+    label: 'Home',
+  },
+];
--- src/app/share/component/sidebar/sidebar.component.ts
+++ src/app/share/component/sidebar/sidebar.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
+import { sideNavData } from './sideNavData';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
   imports: [CommonModule, RouterLink],
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.css']
+  styleUrls: ['./sidebar.component.css'],
 })
 export class SidebarComponent {
-
+  expand: boolean = true;
+  menu = sideNavData;
+  constructor() {}
 }
