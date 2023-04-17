7956c59a0b9d325f0cc60d0fa0cbf80760b8a02a
Pheonixauto <95474402+Pheonixauto@users.noreply.github.com>
2023-04-17 17:40:30 +0700

    3 files modified, 4 added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './component/home/home.component';
+import { PageNotFoundComponent } from './share/component/page-not-found/page-not-found.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'home' },
@@ -11,6 +12,13 @@ const routes: Routes = [
     loadChildren: () =>
       import('./component/vegetables/vegetables-routing.module').then(
         (m) => m.VegetablesRoutingModule
+      ),
+  },
+  {
+    path: '**',
+    loadComponent: () =>
+      import('./share/component/page-not-found/page-not-found.component').then(
+        (c) => c.PageNotFoundComponent
       ),
   },
 ];
--- src/app/share/component/sidebar/sideNavData.ts
+++ src/app/share/component/sidebar/sideNavData.ts
@@ -1,12 +1,12 @@
 export const sideNavData = [
   {
-    routerLink: '/vegetable',
-    icon: '',
-    label: 'Vegetable',
-  },
-  {
     routerLink: '/home',
     icon: '',
     label: 'Home',
+  },
+  {
+    routerLink: '/vegetable',
+    icon: '',
+    label: 'Vegetable',
   },
 ];
