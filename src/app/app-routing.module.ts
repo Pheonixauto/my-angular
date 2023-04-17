58b72f3156e69d84865a8ef1d23f4732073a530c
Pheonixauto <95474402+Pheonixauto@users.noreply.github.com>
2023-04-17 17:42:14 +0700

    1 file modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './component/home/home.component';
-import { PageNotFoundComponent } from './share/component/page-not-found/page-not-found.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'home' },
-  { path: 'home', component: HomeComponent },
+  {
+    path: 'home',
+    loadComponent: () =>
+      import('./component/home/home.component').then((c) => c.HomeComponent),
+  },
   {
     path: 'vegetable',
     loadChildren: () =>
